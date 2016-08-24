f5f7b1094a60bb33ba5f35981e3441d254c31cac
Tommy Waters <Thomas.Waters05@gmail.com>
2016-08-23 19:44:44 -0500

    completed

---
+++ chaining.js
@@ -0,0 +1,35 @@
+// 1.Sort the numbers in descending order (10, 9, 8, 7, etc).
+// 2.Remove any integers greater than 19.
+// 3.Multiply each remaining number by 1.5 and then subtract 1.
+// 4.Then output (either in the DOM or the console) the sum of all the   resulting numbers.
+
+
+
+var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
+console.log(integers.sort(function(a,b){return b-a;})
+  .filter(function(removeThis){return removeThis <= 19})
+  .map(function (math){return (math * 1.5) - 1}));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
